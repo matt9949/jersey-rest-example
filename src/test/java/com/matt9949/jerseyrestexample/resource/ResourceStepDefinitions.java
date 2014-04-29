4472ea233815058dad4bc2319d2d93292241ba32
Matt O'Neill <maoneill@cybersource.com>
2014-04-29 16:37:59 +0100

    Renaming StepDefinitions.java

--- src/test/java/com/matt9949/jerseyrestexample/resource/ResourceStepDefinitions.java
+++ src/test/java/com/matt9949/jerseyrestexample/resource/ResourceStepDefinitions.java
@@ -1,46 +1,63 @@
-package com.matt9949.jerseyrestexample;
+package com.matt9949.jerseyrestexample.resource;
 
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.test.framework.JerseyTest;
-import com.sun.jersey.test.framework.WebAppDescriptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.matt9949.jerseyrestexample.bean.Cat;
+import com.matt9949.jerseyrestexample.service.CatService;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.apache.commons.io.IOUtils;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import com.google.common.io.Resources;
 
-import javax.ws.rs.core.MediaType;
-
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
-public class StepDefinitions extends JerseyTest {
+public class ResourceStepDefinitions {
 
-    public StepDefinitions(){
-        super(new WebAppDescriptor.Builder("com.matt9949.jerseyrestexample.resource")
-                .initParam("com.sun.jersey.api.json.POJOMappingFeature", "true")
-                .build()
-        );
+    private Cat cat;
+    private Response response;
+    private CatResource catResource;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Mock
+    private CatService mockService;
+    @Mock
+    private UriInfo mockUriInfo;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        this.catResource = new CatResource(mockService, mockUriInfo);
     }
 
-    private WebResource webResource = resource();
-    private String requestBody;
-    private ClientResponse response;
+    @Given("^a create cat request \"([^\"]*)\"$")
+    public void a_cat(String catFileName) throws IOException {
+        String jsonCat = IOUtils.toString(getFile(catFileName).toURI());
+        this.cat = mapper.readValue(jsonCat, Cat.class);
+    }
 
-    @Given("^a cat \"([^\"]*)\"$")
-    public void a_cat(String catFilePath) throws IOException {
-        this.requestBody = IOUtils.toString(getFile(catFilePath).toURI());
+    @And("^the create cat request is valid$")
+    public void the_create_cat_request_is_valid() throws URISyntaxException {
+        when(mockService.createCat(cat)).thenReturn("123");
+        when(mockUriInfo.getAbsolutePath()).thenReturn(new URI("test"));
     }
 
     @When("^the client requests to create a cat$")
     public void the_client_requests_to_create_a_cat() {
-        this.response = webResource.path("v1/cats")
-                .type(MediaType.APPLICATION_JSON)
-                .post(ClientResponse.class, requestBody);
+        this.response = catResource.postCat(cat);
     }
 
     @Then("^the response code should be '(\\d+)'$")
@@ -49,7 +66,7 @@ public class StepDefinitions extends JerseyTest {
     }
 
     public File getFile(String fileName) throws IOException {
-        URL url = Thread.currentThread().getContextClassLoader().getResource("com/matt9949/jerseyrestexample/features" + fileName);
+        URL url = Resources.getResource("com/matt9949/jerseyrestexample/requests/" + fileName + ".json");
         return new File(url.getFile());
     }
 }
